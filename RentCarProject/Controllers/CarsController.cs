2a17219e1a565fc15d8a64db110977c81a493fc0
sezgin <erengncuu@outlook.com>
2023-08-03 14:48:58 +0300

    partial view eklendi Crud işlemleri tamamlandı

--- RentCarProject/Controllers/CarsController.cs
+++ RentCarProject/Controllers/CarsController.cs
@@ -13,13 +13,13 @@ namespace RentCarProject.Controllers
 			
 			return View(CarsRepostories.TList());
 		}
-
+		[HttpGet]
 		public IActionResult AddCar()
 		{
 
 			return View();
 		}
-
+		[HttpPost]
 		public IActionResult AddCar(Cars C)
 		{
 			CarsRepostories.TAdd(C);
@@ -28,6 +28,29 @@ namespace RentCarProject.Controllers
 		public IActionResult DeleteCar(int Id)
 		{
 			CarsRepostories.TRemove(new Cars { id = Id });
+			return RedirectToAction("Index");
+		}
+		public IActionResult GetCar(int ID)
+		{
+			var x = CarsRepostories.getT(ID);
+			Cars cr = new Cars()
+			{
+				CarMark = x.CarMark,
+				CarModel= x.CarModel,
+				ModelYear=x.ModelYear,
+				Price=x.Price,
+			};
+			return View(x);
+		}
+		[HttpPost]
+		public IActionResult UpdateCar(Cars cr)
+		{
+			var x = CarsRepostories.getT(cr.id);
+			x.CarMark = cr.CarMark;
+			x.CarModel = cr.CarModel;
+			x.ModelYear = cr.ModelYear;
+			x.Price = cr.Price;
+			CarsRepostories.TUpdate(x);
 			return RedirectToAction("Index");
 		}
 	}
